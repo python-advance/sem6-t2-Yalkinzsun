9ba03eea00bd1747c35e06b39583ee44710e1f61
Kirill Skorobogatov <Kirill9806@gmail.com>
2019-06-26 12:11:37 +0300

    Update test_main.py

--- Вариативные задания/test_main.py
+++ Вариативные задания/test_main.py
@@ -3,8 +3,9 @@ from main import fib_infinite_iterator
 
 
 @pytest.mark.parametrize("n, result", [
+    (-100, None),
     (1, [0]),
-    (5, [0, 0, 1, 2, 3, 5, 8]),
-    (10, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89])])
+    (5, [0, 1, 1, 2, 3]),
+    (10, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])])
 def test_fib_infinite_iterator(n, result):
     assert fib_infinite_iterator(n) == result
